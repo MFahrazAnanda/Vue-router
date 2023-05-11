a5c83cbb88ef5c98a194257b32cbee86ba222a6a
MFahrazAnanda <fahrazananda1212@gmail.com>
2023-05-11 21:33:59 +0700

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="contact">
+        <div class="wrapper">
+            <div class="footer">
+                <div class="footer-section">
+                    <h3>KepoNihon.</h3>
+                    <p>Halo, teman-teman Indonesia!</p>
+                    <p><em>Mari kita membuat to do list kita sehari-hari</em><br>dengan menggunakan blognya dan juga follow Sosial Medianya!</p>
+                    <p>Yoroshiku!</p>
+                </div>
+                <div class="footer-section">
+                    <h3>About</h3>
+                    <p>KepoNihon. adalah website yang berisi to do list, Hal ini dapat digunakan untuk tugas harian, tugas mingguan, tugas bulanan, dan bahkan tugas tahunan. To do list sangat penting karena membantu kita mengatur waktu dan memastikan bahwa kita menyelesaikan pekerjaan tepat waktu.</p>
+                </div>
+                <div class="footer-section">
+                    <h3>Contact</h3>
+                    <p>KepoNihon.@gmail.com</p>
+                </div>
+                <div class="footer-section">
+                    <h3>Social Media</h3>
+                    <p>Instagram : @KepoNihon.</p>
+                    <p>Facebook : KepoNihon.</p>
+                    <p>Youtube : KepoNihon.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="copyright">
+        <div class="wrapper">
+            &copy; 2023. <b>KepoNihon.</b>
+        </div>
+    </div>
+</template>
+
+<style>
+#contact {
+    background: #dedede;
+    padding: 50px 0px 50px 0px;
+}
+
+.footer {
+    width: 100%;
+    position: relative;
+    display: flex;
+    flex-wrap: wrap;
+    margin: auto;
+}
+
+.footer-section {
+    width: 20%;
+    margin: 0 auto;
+}
+
+h3 {
+    font-family: 'comic sans ms';
+    font-weight: 800;
+    font-size: 30px;
+    margin-bottom: 20px;
+    color: #4CAF50;
+    width: 100%;
+    line-height: 50px;
+}
+
+#copyright {
+    text-align: center;
+    width: 100%;
+    padding: 50px 0px 50px 0px;
+    margin-top: 50px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,174 @@
+<template>
+  <div class="container">
+    <h1>Selamat Datang</h1>
+    <h1>To Do List </h1>
+    <div class="add-todo">
+      <input v-model="newTodo" placeholder="Tambahkan Kegiatan">
+      <button @click="addTodo">Tambah Kegiatan</button>
+    </div>
+    <div class="filter-todo">
+      <button @click="hideCompleted = !hideCompleted">{{ hideCompleted ? 'Tampilkan Semua Kegiatan' : 'Sembunyikan yang sudah selesai' }}</button>
+    </div>
+    <ul class="todo-list">
+      <li v-for="todo in visibleTodos" :key="todo.id">
+        <span :class="{ 'completed': todo.completed }" @click="toggleComplete(todo)">{{ todo.text }}</span>
+        <div class="todo-buttons">
+          <button v-if="!todo.completed" @click="completeTodo(todo.id)">Selesai</button>
+          <button v-if="todo.completed" @click="unCompleteTodo(todo.id)">Belum Selesai</button>
+          <button @click="removeTodo(todo.id)">Hapus</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+  <div id="copyright">
+        <div class="wrapper">
+            &copy; 2023. <b>KepoNihon.</b>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      todos: [],
+      newTodo: '',
+      nextId: 1,
+      hideCompleted: false
+    }
+  },
+  computed: {
+    visibleTodos() {
+      if (this.hideCompleted) {
+        return this.todos.filter(todo => !todo.completed);
+      } else {
+        return this.todos;
+      }
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim() !== '') {
+        this.todos.push({
+          id: this.nextId++,
+          text: this.newTodo,
+          completed: false
+        });
+        this.newTodo = '';
+      }
+    },
+    removeTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    completeTodo(id) {
+      const todo = this.todos.find(todo => todo.id === id);
+      if (todo) {
+        todo.completed = true;
+      }
+    },
+    unCompleteTodo(id) {
+      const todo = this.todos.find(todo => todo.id === id);
+      if (todo) {
+        todo.completed = false;
+      }
+    },
+    toggleComplete(todo) {
+      todo.completed = !todo.completed;
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 20px;
+  font-family: Arial, sans-serif;
+}
+
+h1 {
+  font-size: 36px;
+  font-weight: bold;
+  text-align: center;
+}
+
+.add-todo {
+  display: flex;
+  margin-bottom: 20px;
+}
+
+.add-todo input {
+  flex-grow: 1;
+  padding: 10px;
+  font-size: 16px;
+  border: 2px solid #ccc;
+  border-radius: 5px;
+}
+
+.add-todo button {
+  margin-left: 10px;
+  padding: 10px;
+  font-size: 16px;
+  background-color: #4CAF50;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.filter-todo {
+  text-align: right;
+  margin-bottom: 20px;
+}
+
+.filter-todo button {
+  padding: 
+  10px;
+  font-size: 14px;
+  background-color: #2196F3;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.todo-list {
+  list-style: none;
+  padding: 0;
+}
+
+li {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+  padding: 10px;
+  background-color: #f9f9f9;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+
+.completed {
+  text-decoration: line-through;
+}
+
+.todo-buttons button {
+  padding: 5px 10px;
+  margin-right: 5px;
+  font-size: 14px;
+  background-color: #f44336;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+#copyright {
+    text-align: center;
+    width: 100%;
+    padding: 50px 0px 50px 0px;
+    margin-top: 50px;
+}
+</style>
